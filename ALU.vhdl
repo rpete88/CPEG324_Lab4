3a80649bcb58b21d9ebcd76609228bdc4fd5e487
rpete88 <rfpeterson2@gmail.com>
2020-05-11 22:05:43 -0400

    started ALU and instruction reader

--- ALU.vhdl
+++ ALU.vhdl
@@ -11,13 +11,31 @@ use ieee.std_logic_1164.all;
 entity ALU is
 	port(	A:	in std_logic_vector(31 downto 0); --first input
 		B:	in std_logic_vector(31 downto 0); --second input
-		op:	in std_logic_vector(1 downto 0); --operation
-		o:	out std_logic_vector(31 downto 0); --output
-		eq:	out std_logic
+		OP:	in std_logic_vector(1 downto 0); --operation- 00:add; 01:sub; 10:nothing; 11:set on equal
+		O:	out std_logic_vector(31 downto 0); --output
+		EQ:	out std_logic --set on equal output
 	);
 end ALU;
 
 architecture behav of ALU is
 begin
-
+	process(A, B, OP)
+	begin
+		case OP is
+			when "00" =>
+				O <= A + B;
+			when "01" =>
+				O <= A - B;
+			when "10" =>
+				-- do nothing
+			when "11" =>
+				if (A = B) then
+					EQ = '1';
+				else
+					EQ = '0';
+				end if;
+			when others =>
+				-- do nothing
+		end case;
+	end process;
 end behav;
---
+++ instructionReader.vhdl
@@ -0,0 +1,36 @@
+------------------------------------
+-- Instrcution Reader
+-- by Alex Bushinsky and Ryan Peterson
+-- Lab4 part 2- CPEG324
+-- Logic component that translates instructions from our 
+-- ISA into the correct OP values for ALU
+--------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity instructionReader is
+	port(	I:	in std_logic_vector(7 downto 0); --instruction sent
+		op:	out std_logic_vector(1 downto 0) --opcode sent to ALU 00:add; 01:sub; 10:nothing; 11:set on equal
+	);
+end instructionReader;
+
+architecture behav of instructionReader is
+begin
+	process(I)
+	begin
+		case (I (7 downto 6)) is
+			when "00" => --opcode for add
+				op <= "00";
+			when "01" => --opcode for sub
+				op <= "01";
+			when "11" => --opcode for beq and dis
+				if( I(0) = '1') then --funct set to 1 for beq
+					op <= "11";
+				else --funct set to 0 for dis
+					op <= "10";
+			when others =>
+				op <= "10";
+		end case;
+	end process;
+end behav;
